fbc73a4535d77e297a95890c980e752823b487c2
Pavle <paja98100@gmail.com>
2021-01-16 20:23:00 +0100

    changing some functions to ES6 syntax

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const side = document.getElementById("side");
 let setGradient = () => {
     bg.style.background = _linearGradient();
     output.textContent = `${bg.style.background};`
-}
+};
+
+let randomClr = () => {
+    input1.value = `#${(Math.random()*0xFFFFFF << 0).toString(16).padStart(6, '0')}`;
+    input2.value = `#${(Math.random()*0xFFFFFF << 0).toString(16).padStart(6, '0')}`;
+    setGradient();
+};
 
 input1.addEventListener('input', setGradient);
 input2.addEventListener('input', setGradient);
@@ -18,14 +24,7 @@ side.addEventListener('change', setGradient);
 let _linearGradient = () => {
     return `linear-gradient(${side.options[side.selectedIndex].value}, ${input1.value}, ${input2.value}) `;
 
-}
-
-function randomClr() {
-    input1.value = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-
-    input2.value = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-    setGradient();
-}
+};
 
 
 setGradient();
